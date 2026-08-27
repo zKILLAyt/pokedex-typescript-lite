6f5f909f0ecc6acebd979d8aa39c9f888eb93a7c
Gustavo Gomes <ferreiragu84@gmail.com>
2026-08-27 13:53:07 -0300

    refactor: organiza arquitetura da aplicacao

--- src/main.ts
+++ src/main.ts
@@ -1,41 +1,17 @@
-import { PokeApiService } from "./services/PokeApiService";
+import { TerminalController } from "./controllers/TerminalController";
 import { CatalogoPokemon } from "./models/CatalogoPokemon";
+import { PokeApiService } from "./services/PokeApiService";
 
 async function main(): Promise<void> {
   const pokeApiService = new PokeApiService();
   const catalogo = new CatalogoPokemon();
 
-  const pikachu = await pokeApiService.buscarPokemon("pikachu");
-
-  if (pikachu !== null) {
-    catalogo.adicionar(pikachu);
-  }
-
-  console.log(pikachu);
-
-  const charmander = await pokeApiService.buscarPokemon("charmander");
-
-  if (charmander !== null) {
-    catalogo.adicionar(charmander);
-  }
-
-  const pikachuDuplicado = await pokeApiService.buscarPokemon("pikachu");
-
-  if (pikachuDuplicado !== null) {
-    catalogo.adicionar(pikachuDuplicado);
-  }
-
-  const pokemonInexistente = await pokeApiService.buscarPokemon(
-    "pokemon-inexistente",
+  const terminalController = new TerminalController(
+    pokeApiService,
+    catalogo,
   );
 
-  console.log(pokemonInexistente);
-
-  catalogo.listar();
-
-  catalogo.remover(25);
-
-  catalogo.listar();
+  await terminalController.executar();
 }
 
 main();
